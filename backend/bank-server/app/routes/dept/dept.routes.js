c19ac840f7c6ec61ec5a550496be66a822eecca5
DESKTOP-ABSTO8J\E7450 <adleomalik@gmail.com>
2023-01-27 15:52:14 +0700

    delete backend

--- backend/bank-server/app/routes/dept/dept.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-  const dept = require("../../controllers/dept/dept.controller.js");
-  // const keycloak = require('../../config/keycloak.config').initKeycloak();
-
-  var router = require("express").Router();
-  router.get("/", dept.getAllDeptForOption);
-  router.get("/find-all", dept.getAllDeptForOption);
-  app.use("/api/dept/", router);
-};
